64ebdb58953daf60ca6c26c674f82d1ca66f5e0b
Nur Izzat Hazman <nurizzathazman1@gmail.com>
2026-05-07 17:31:03 +0800

    Add calculator and test files

---
+++ test_calculator.py
@@ -0,0 +1,12 @@
+# test_calculator.py
+
+from calculator import multiply
+
+def test_multiply_positive_numbers():
+    assert multiply(2, 3) == 6
+
+def test_multiply_with_zero():
+    assert multiply(5, 0) == 0
+
+def test_multiply_negative_numbers():
+    assert multiply(-2, 3) == -6
